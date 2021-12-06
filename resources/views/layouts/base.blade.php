263ecad050d1ab5513d74fad9117883ba5ac4a77
BekhanBigashev <bigashev25@gmail.com>
2021-12-06 20:38:17 +0600

    6.12

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -26,7 +26,7 @@
 
  		<!-- Custom stlylesheet -->
  		<link type="text/css" rel="stylesheet" href="/css/style.css"/>
-
+        asdad
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
@@ -190,7 +190,7 @@
         @endif
 		<!-- NAVIGATION -->
         <nav id="navigation">
-			
+
 			<div class="container">
 				<!-- responsive-nav -->
 				<div id="responsive-nav">
