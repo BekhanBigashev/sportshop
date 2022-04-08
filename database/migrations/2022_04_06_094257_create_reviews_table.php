5da0284349988422e1c1892e6989b741e4d67fcd
Bekkhan <62833581+BekhanBigashev@users.noreply.github.com>
2022-04-08 14:43:44 +0600

    second branch

---
+++ database/migrations/2022_04_06_094257_create_reviews_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('product_id')->unsigned()->nullable();
+            $table->string('text');
+            $table->string('name');
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->integer('score');
+            $table->timestamps();
+
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->nullOnDelete();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
